e9b8515363b26a6fe75e8e6fa2d274cd2c3c829d
Irshad Ansari <Irshad.Ansari41@gmail.com>
2019-01-02 10:08:26 +0400

    show Areas on home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,46 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {HttpClientModule} from '@angular/common/http';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
+import {ListPage} from '../pages/list/list';
+import {PropertyPage} from '../pages/property/property';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {PropertyProvider} from '../providers/property/property';
+import {KeysPipe} from '../pipes/keys/keys'
+
+
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage,
-    ListPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp),
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage,
-    ListPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [
+        MyApp,
+        HomePage,
+        ListPage,
+        PropertyPage,
+        KeysPipe
+    ],
+    imports: [
+        HttpClientModule,
+        BrowserModule,
+        IonicModule.forRoot(MyApp),
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage,
+        ListPage,
+        PropertyPage,
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        {provide: ErrorHandler, useClass: IonicErrorHandler},
+        PropertyProvider
+    ]
 })
+
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,44 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {PropertyProvider} from '../../providers/property/property';
+
+import {PropertyPage} from '../property/property';
+
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    public areas: object = [];
+    public propertyPage=PropertyPage;
 
-  }
+    constructor(public navCtrl: NavController, private propertyProvider: PropertyProvider) {
+        this.getArea();
+
+    }
+
+    getArea() {
+        this.propertyProvider.getProjects().subscribe((areas) => {
+            this.areas = areas;
+            console.log(areas);
+        });
+
+    }
+    
+     openPropertyPage(area) {
+        // Reset the content nav to have just this page
+        // we wouldn't want the back button to show in this scenario
+        this.navCtrl.push(PropertyPage,{area:area});
+    }
+    
+    openCommunityPage() {
+        // Reset the content nav to have just this page
+        // we wouldn't want the back button to show in this scenario
+        this.navCtrl.push(PropertyPage);
+    }
+
+
 
 }
